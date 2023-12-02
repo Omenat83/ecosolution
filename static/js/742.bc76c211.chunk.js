5a43a750f5a1c19b2195ce664b79f99dd2968b75
Omenat83 <110280285+Omenat83@users.noreply.github.com>
2023-12-02 17:29:04 +0000

    Deploying to gh-pages from @ Omenat83/ecosolution@e9307135d593300e703cfd30e639c3988f7a379e 🚀

---
+++ static/js/742.bc76c211.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[742],{742:function(e,t,a){e.exports=a.p+"static/media/spriteSvg.3c3ba004c1048e579788.svg"}}]);
